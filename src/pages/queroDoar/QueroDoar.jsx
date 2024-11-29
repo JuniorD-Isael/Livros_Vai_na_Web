610138a035d638a6b44888e00ea327367ebd2aa8
JuniorD-Isael <isaeldjunior@gmail.com>
2024-11-29 18:07:27 -0300

    style:Iniciada e estilizada a pagina 'Quero doar'

--- src/pages/queroDoar/QueroDoar.jsx
+++ src/pages/queroDoar/QueroDoar.jsx
@@ -1,7 +1,21 @@
+import logoForm from '../../assets/form-livro.png';
+import s from './queroDoar.module.scss';
+
 export const QueroDoar = () => {
     return (
-        <div>
-            <h1>Quero doar</h1>
-        </div>
+        <section className={s.queroDoarSection}>
+            <p>Por favor preencha o formulário abaixo com as suas informações.</p>
+            <form action="">
+                <div>
+                    <img src={logoForm} alt="" />
+                    <h2>Informações do Livro</h2>
+                </div>
+                <input type="text" id='' placeholder='Titulo'/>
+                <input type="text" id='' placeholder='Categoria'/>
+                <input type="text" id='' placeholder='Autor'/>
+                <input type="text" id='' placeholder='Link da Imagem'/>
+                <input type="submit" value="Doar" className={s.buttonDoar}/>
+            </form>
+        </section>
     );
 }
