ea8a7926f624cf2d9df4b304616893ecbdce8d31
JuniorD-Isael <isaeldjunior@gmail.com>
2024-11-29 19:30:26 -0300

    style:Implementada e estilizada a pagina 'Livros Doados' e iniciado a parte de responsividade

--- src/pages/livrosDoados/LivrosDoados.jsx
+++ src/pages/livrosDoados/LivrosDoados.jsx
@@ -1,7 +1,20 @@
+import livroProtagonista from '../../assets/livroProtagonista.png';
+import s from './livrosDoados.module.scss';
+
 export const LivrosDoados = () => {
     return (
-        <div>
-            <h1>Livros doados</h1>
-        </div>
+        <section className={s.livrosDoadosSection}>
+            <h2>Livros Doados</h2>
+            <section className={s.containerCards}>
+                <section>
+                    <div>
+                        <img src={livroProtagonista} alt="" />
+                        <h3>O protagonista</h3>
+                        <p>Susanne Andrade</p>
+                        <p>Ficção</p>
+                    </div>
+                </section>
+            </section>
+        </section>
     );
 }
